b3e00944688b93c60d0f2558ea71cec06f9fe6ca
DEVENDRA KUMAR KEVAT <136752283+imdevendra01@users.noreply.github.com>
2024-10-23 16:12:49 +0530

    Add files via upload

---
+++ Apple_Redistribution_into_Boxes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+//import java.util.collections.*;
+public class Apple_Redistribution_into_Boxes {
+    public static void main(String[] args) {
+       int[] arr ={54,65,9,11,7,2,3};
+      Arrays.sort(arr);//
+      // Arrays.sort(arr,Collections.reverseOrder());
+       for(int i:arr){
+        System.out.println(i);
+       }
+        
+    }
+}
